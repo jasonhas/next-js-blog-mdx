ce18e48c5dc2242313203c05af3e4340c8a5de26
Jason Hasselkus <jason.hasselkus@gmail.com>
2023-09-10 11:40:56 -0500

    Added About Page

---
+++ app/about/page.js
@@ -0,0 +1,10 @@
+const About = () => {
+
+    return (
+        <div className="mx-auto">
+            <h1 className="text-3xl font-bold">About Me</h1>
+        </div>
+    )
+}
+
+export default About
